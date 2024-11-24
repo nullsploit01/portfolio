ea664f7f4ebf3b1921bfbda8300f4776a0fbd2c9
nullsploit01 <harshaldharmik099@gmail.com>
2024-11-24 21:08:00 +0530

    add prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import "./App.css";
+import './App.css'
 
 const App = () => {
   return (
     <div className="work-in-progress-container">
       <div className="animation-wrapper">
-        {"Coming Soon".split("").map((char, index) => (
-          <span key={index} className={`letter ${char === " " ? "space" : ""}`}>
+        {'Coming Soon'.split('').map((char, index) => (
+          <span key={index} className={`letter ${char === ' ' ? 'space' : ''}`}>
             {char}
           </span>
         ))}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
