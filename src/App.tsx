828405b3f9bb4e20c64e1ee083771487f0cfabee
nullsploit01 <harshaldharmik099@gmail.com>
2024-11-25 22:12:09 +0530

    install framer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import './App.css'
+import { motion } from 'motion/react'
 
 const App = () => {
   return (
-    <div className="work-in-progress-container">
-      <div className="animation-wrapper">
-        {'Coming Soon'.split('').map((char, index) => (
-          <span key={index} className={`letter ${char === ' ' ? 'space' : ''}`}>
-            {char}
-          </span>
-        ))}
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
+    >
+      <div className="work-in-progress-container">
+        <div className="animation-wrapper">
+          {'Coming Soon'.split('').map((char, index) => (
+            <span key={index} className={`letter ${char === ' ' ? 'space' : ''}`}>
+              {char}
+            </span>
+          ))}
+        </div>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
