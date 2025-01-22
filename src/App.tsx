b9997fbf8f310342c793a56d73f4c6e672bb4d9f
nullsploit01 <harshaldharmik099@gmail.com>
2025-01-23 02:50:30 +0530

    add ability to view resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import './App.css';
+import { Download } from 'lucide-react';
 
+import { Button } from '@/components/ui/button';
 import company from '@/constants/company';
 import self from '@/constants/self';
 
 const App = () => {
   return (
-    <div className="flex min-h-screen justify-center items-center flex-col">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ">
-        {self.NAME}
-      </h1>
-      <p>
-        {company.JOBTITLE}{' '}
-        <a href="https://www.linkedin.com/company/bushel-technologies-private-limited/">
-          @{company.SHORT_NAME}
-        </a>
-      </p>
+    <div>
+      <div className="flex min-h-screen justify-center items-center flex-col">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ">
+          {self.NAME}
+        </h1>
+        <p>
+          {company.JOBTITLE}{' '}
+          <a href="https://www.linkedin.com/company/bushel-technologies-private-limited/">
+            @{company.SHORT_NAME}
+          </a>
+        </p>
+        <Button asChild size="lg" variant="ghost">
+          <a target="_blank" href={self.RESUME}>
+            <Download />
+            Resume
+          </a>
+        </Button>
+      </div>
     </div>
   );
 };
--- src/constants/self/index.ts
+++ src/constants/self/index.ts
@@ -3,7 +3,8 @@ const self = {
   EMAIL: 'harshaldharmik099@gmail.com',
   PHONE: '+91 7414994939',
   LINKEDIN: 'https://www.linkedin.com/in/harshal-dharmik-44a1001a8/',
-  GITHUB: 'https://github.com/nullsploit01'
+  GITHUB: 'https://github.com/nullsploit01',
+  RESUME: 'https://drive.google.com/file/d/1Vag6FOAT055cMaG1W4-MCqzpieAZMU12/view?usp=sharing'
 };
 
 export default self;
