4c43db0cc71d1b9325ed77405e6dc5ed322bda17
nullsploit01 <harshaldharmik099@gmail.com>
2025-01-23 02:34:35 +0530

    remove extra code

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
 import './App.css';
-import { useMotionValue } from 'motion/react';
-import { useEffect } from 'react';
 
 import company from '@/constants/company';
 import self from '@/constants/self';
 
 const App = () => {
-  const x = useMotionValue(0);
-
-  useEffect(() => {
-    const timeout = setTimeout(() => x.set(50), 1000);
-
-    return () => clearTimeout(timeout);
-  }, []);
-
   return (
     <div className="flex min-h-screen justify-center items-center flex-col">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ">
