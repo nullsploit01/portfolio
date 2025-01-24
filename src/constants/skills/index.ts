5b324b6682aa12cd1db4e471b760360d95e34cad
nullsploit01 <harshaldharmik099@gmail.com>
2025-01-24 18:46:28 +0530

    add skills in constants

---
+++ src/constants/skills/index.ts
@@ -0,0 +1,20 @@
+const skills = {
+  FRONTEND: ['HTML', 'CSS', 'JavaScript', 'TypeScript', 'React'],
+  BACKEND: [
+    'Node.js',
+    'Express',
+    'MongoDB',
+    'GraphQL',
+    'Golang',
+    'Python',
+    'C#',
+    'DotNet',
+    'SQL Server',
+    'PostgreSQL',
+    'MySQL',
+    'Redis'
+  ],
+  DEVOPS: ['Docker', 'GCP', 'Azure', 'Git', 'GitHub', 'Firebase']
+};
+
+export default skills;
