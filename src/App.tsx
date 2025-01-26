44a1e409f894777925b9035cba979a87d4a8dbb3
nullsploit01 <harshaldharmik099@gmail.com>
2025-01-27 02:15:47 +0530

    add a component to animate basic elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css';
 import { Download } from 'lucide-react';
 
+import AnimatedContent from '@/components/animated/animated-content';
 import { Button } from '@/components/ui/button';
 import company from '@/constants/company';
 import self from '@/constants/self';
 
 const App = () => {
   return (
-    <div>
+    <div className="mx-8">
       <div className="flex min-h-screen justify-center items-center flex-col">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl ">
           {self.NAME}
@@ -25,7 +26,22 @@ const App = () => {
           </a>
         </Button>
       </div>
-      <h3>My Skills</h3>
+      <div>
+        <AnimatedContent
+          distance={150}
+          direction="horizontal"
+          reverse={true}
+          config={{ tension: 80, friction: 20 }}
+          initialOpacity={0.2}
+          animateOpacity
+          scale={1.1}
+          threshold={0.2}
+        >
+          <h3 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+            My Skills
+          </h3>
+        </AnimatedContent>
+      </div>
     </div>
   );
 };
