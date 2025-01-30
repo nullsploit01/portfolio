f039650107812acf2514aa4059dfd51d937db0cd
nullsploit01 <harshaldharmik099@gmail.com>
2025-01-30 20:28:16 +0530

    add hover effect on techbox

--- src/components/custom/tech-box/index.tsx
+++ src/components/custom/tech-box/index.tsx
@@ -7,7 +7,7 @@ interface TechBoxProps {
 
 const TechBox: React.FC<TechBoxProps> = ({ icon, name }) => {
   return (
-    <div className="flex items-center space-x-3 px-6 py-3 border border-gray-700 rounded-md">
+    <div className="flex items-center space-x-3 px-6 py-3 border border-gray-700 rounded-md transition-all duration-200 transform hover:scale-105 hover:brightness-125 hover:shadow-lg hover:shadow-gray-700">
       <img src={icon} alt={name} className="w-6 h-6" />
       <span className="text-white font-semibold text-lg">{name}</span>
     </div>
